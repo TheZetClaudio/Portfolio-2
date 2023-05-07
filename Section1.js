d08f2c1a9b15473d9aba320d79949e093046669f
TheZetClaudio <zeter54.klaudiusz@gmail.com>
2023-05-07 22:29:55 +0200

    Update(Navbar, Section1, Aside)

---
+++ Section1.js
@@ -0,0 +1,22 @@
+const dot1 = document.querySelector(".dot1");
+const dot2 = document.querySelector(".dot2");
+const Call1 = document.querySelector("#FirstCall");
+const Call2 = document.querySelector("#SecondCall");
+
+const changeFirstSlide = () => {
+    Call2.classList.add("SecondCall");
+    Call2.classList.remove('SecondCallAfter');
+    Call1.classList.add("FirstCallAfter");
+    Call1.classList.remove("FirstCall")
+    
+};
+dot2.addEventListener('click', changeFirstSlide);
+
+const changeSecondSlide = () => {
+    Call2.classList.remove("SecondCall");
+    Call2.classList.add('SecondCallAfter');
+    Call1.classList.remove("FirstCallAfter");
+    Call1.classList.add("FirstCall")
+    
+};
+dot1.addEventListener('click', changeSecondSlide);
